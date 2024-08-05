869ba31ac63b15f88377c0eaa9254af8193e2311
saltcute <me@mizuno.ai>
2024-08-05 03:51:09 -0400

    feat: allow custom appName

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import StatusCommand from "@saltcute/kasumi-status";
 import Kasumi, { BaseMenu } from "kasumi.js";
 
 class EssentialMenu extends BaseMenu {
-    constructor(name: string = "essential") {
+    constructor(
+        name: string = "essential",
+        private appName = "kasumi.js"
+    ) {
         super();
         this.name = name;
     }
 
     init(client: Kasumi<any>, loggerSequence: string[]) {
         super.init(client, loggerSequence);
-        this.load(new PingCommand(), new StatusCommand());
+        this.load(new PingCommand(), new StatusCommand("status", this.appName));
     }
 }
 
